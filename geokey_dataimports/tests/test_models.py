2341b692ea807dbd7b37cb83ff6aee7dc8880625
Julius Osokinas <j.osokinas@mappingforchange.org.uk>
2016-03-17 11:34:02 +0000

    Import contributions of data import

--- geokey_dataimports/tests/test_models.py
+++ geokey_dataimports/tests/test_models.py
@@ -10,6 +10,7 @@ from geokey.projects.models import Project
 from geokey.projects.tests.model_factories import ProjectFactory
 from geokey.categories.models import Category
 from geokey.categories.tests.model_factories import CategoryFactory
+from geokey.contributions.models import Observation
 
 from .model_factories import DataImportFactory
 from ..models import DataImport, post_save_project, post_save_category
@@ -34,6 +35,37 @@ class DataImportTest(TestCase):
         self.file = dataimport.file.path
         dataimport.delete()
         DataImport.objects.get(pk=dataimport.id)
+
+    def test_import_contributions(self):
+        """Test import contributions of data import."""
+        dataimport = DataImportFactory.create()
+        ids = [datafeature.id for datafeature in dataimport.datafeatures.all()]
+        response = dataimport.import_contributions(dataimport.creator, ids)
+        self.assertEqual(response, [])
+        self.assertEqual(Observation.objects.all(), 3)
+
+    def test_import_contributions_when_ids_not_set(self):
+        """Test import contributions when IDs are not set."""
+        dataimport = DataImportFactory.create()
+        response = dataimport.import_contributions(dataimport.creator, [])
+        self.assertEqual(response, [])
+        self.assertEqual(Observation.objects.all(), 0)
+
+    def test_import_contributions_when_category_not_set(self):
+        """Test import contributions when category fields are not set."""
+        dataimport = DataImportFactory.create(category=None)
+        ids = [datafeature.id for datafeature in dataimport.datafeatures.all()]
+        response = dataimport.import_contributions(dataimport.creator, ids)
+        self.assertEqual(len(response), 3)
+        self.assertEqual(Observation.objects.all(), 0)
+
+    def test_import_contributions_when_category_fields_not_set(self):
+        """Test import contributions when category fields are not set."""
+        dataimport = DataImportFactory.create()
+        ids = [datafeature.id for datafeature in dataimport.datafeatures.all()]
+        response = dataimport.import_contributions(dataimport.creator, ids)
+        self.assertEqual(len(response), 3)
+        self.assertEqual(Observation.objects.all(), 0)
 
 
 class PostSaveProjectTest(TestCase):
