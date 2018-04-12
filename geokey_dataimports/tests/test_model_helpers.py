6e8e7a067419166afd632aa63ecb743dd6c3a162
JamesBradbury <github@jamesbradbury.co.uk>
2018-04-12 18:32:27 +0100

    Simplify test data for easier comparison.

--- geokey_dataimports/tests/test_model_helpers.py
+++ geokey_dataimports/tests/test_model_helpers.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from io import BytesIO
+from cStringIO import StringIO
 from django.test import TestCase
 
 
@@ -13,16 +13,19 @@ class ImportFromCSVTest(TestCase):
     """
     def test_import_csv_basic_chars(self):
         """Basic ASCII characters can be imported."""
-        mock_csv = BytesIO("abc,cde,efg\n123,456,789")
+        input_dict = {u'abc': u'123', u'cde': u'456', u'efg': u'789'}
+        mock_csv = StringIO("abc,cde,efg\n123,456,789")
         features = []
-        import_from_csv(features=features, fields=[], file=mock_csv)
-        print(features)
-        self.assertEquals(features[0]['properties'], {'cde': '456', 'abc': '123', 'efg': '789'})
+        import_from_csv(features=features, fields=[], file_obj=mock_csv)
+        for k, v in input_dict.items():
+            self.assertEquals(v, features[0]['properties'][k])
 
     def test_import_csv_non_ascii_chars(self):
         """Non-ASCII unicode characters can be imported."""
-        mock_csv = BytesIO("abc,àde,e£g\n¡23,45Ç,Æ8é")
+        input_dict = {u'à': u'¡', u'£': u'Ç'}
+        mock_csv = StringIO("à,£\n¡,Ç")
         features = []
-        import_from_csv(features=features, fields=[], file=mock_csv)
-        print(features)
-        self.assertEquals(features[0]['properties'], {'àde': '45Ç', 'abc': '¡23', 'e£g': 'Æ8é'})
+        import_from_csv(features=features, fields=[], file_obj=mock_csv)
+        for k, v in input_dict.items():
+            self.assertEquals(v, features[0]['properties'][k])
+
